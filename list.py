7cb8ce274cef5960face0dd8f7ef987f75d8323b
aks7665 <aks199628@gmail.com>
2018-01-25 10:15:29 +0530

    first commit

---
+++ list.py
@@ -0,0 +1,39 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:54:40) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # list
+>>> cap = ['a',12,'bg']
+>>> cap
+['a', 12, 'bg']
+>>> cap[1]
+12
+>>> cap[1:2]
+[12]
+>>> cap[1] = 20
+>>> cap[1]
+20
+>>> cap
+['a', 20, 'bg']
+>>> cap.append("csr")
+>>> cap
+['a', 20, 'bg', 'csr']
+>>> emptyList = []
+>>> emptyList
+[]
+>>> cap[-1]
+'csr'
+>>> cap[:3]
+['a', 20, 'bg']
+>>> cap[1:-1]
+[20, 'bg']
+>>> cap
+['a', 20, 'bg', 'csr']
+>>> x = [[1,2,3,4],["a","b","c"]]
+>>> x
+[[1, 2, 3, 4], ['a', 'b', 'c']]
+>>> len(cap)
+4
+>>> cap
+['a', 20, 'bg', 'csr']
+>>> 'i' in 'amit'
+True
+>>> 
